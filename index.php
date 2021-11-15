089ef2f7cf1ed4b6f9b87f7b52d20e57d586413c
Avantika <avantikagulhane99@gmail.com>
2021-11-15 16:14:35 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <!-- Latest compiled and minified CSS -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+     <link href="style.css" type ="text/css" rel="stylesheet">
+     <!-- jQuery library -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+     <!-- Latest compiled and minified JavaScript -->
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+  <?php include "navbar.php"?>
+  <!-- image -->
+  <div class =image>
+      <div class="text">
+       <h1 > Welcome to The World Bank</h1>
+       <div class=b> <button style="background-color:rgb(240, 93, 8)"><a href="transfermoney.php" style="color:white">Transfer Money</a></button>
+       </div>
+    </div></div>
+<!-- footer -->
+<div class="footer">
+ <center>
+      <p> Copyright © The World Bank,Made by Avantika Gulhane All Rights
+Reserved ​and ​Contact Us: +91 90000 00000
+</p>    </center>
+  </div>
+</body>
+</html>
+   
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,38 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <!-- Latest compiled and minified CSS -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+      <link href="style.css" type ="text/css" rel="stylesheet">
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  
+  
+      <!-- Latest compiled and minified JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+ </head>
+ <body>
+     <!-- navbar -->
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">The World Bank</a>
+         </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Home</a></li>
+          <li><a href="transfermoney.php"> Our Customers</a></li>
+          <li><a href="transactionhistory.php"> Transaction History</a></li>
+    
+         </ul></div>
+    </nav>
+ </body>
+ </html>
+  
+
+
+
+
